0aa6948a88d3e1c142d6e021de967db3e2ebc308
Adriana Black <adrianablancov@gmail.com>
2025-09-17 21:26:28 +0100

    set up front end

---
+++ index.js
@@ -0,0 +1,35 @@
+// Court 
+//2 or 4 players
+
+// if two players they can be:
+//  male v male
+//  female v female
+//  male v female
+
+// if FOUR players they can be:
+//  4 same gender
+//  female / male vs female/male
+
+
+//Player
+
+//Name:
+// gender:
+
+const players = [{
+    name: 'Adriana Black',
+    gender: 'female'
+},
+{
+    name: 'Robbie Black',
+    gender: 'male'
+},
+{
+    name: 'Billy Jean',
+    gender: 'female'
+},
+{
+    name: 'Andy Murray',
+    gender: 'male'
+}
+]
